b19dea410b3a50631a3d599886e116d3dbbe6e61
Roberson Alves <robersonjfa@gmail.com>
2025-08-24 20:47:31 -0300

    Adicionada conf para versel

--- ts/tastyboard-api/src/main.ts
+++ ts/tastyboard-api/src/main.ts
@@ -1,17 +1,19 @@
+// src/main.ts
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true, transform: true }));
-
-  const prisma = app.get(PrismaService);
-  await prisma.enableShutdownHooks(app);
-
   await app.listen(3001);
-  console.log('🚀 TastyBoard API em http://localhost:3001');
+  console.log('🚀 API local em http://localhost:3001');
 }
-bootstrap();
+
+// Evita iniciar servidor ao rodar como Serverless na Vercel
+if (!process.env.VERCEL) {
+  bootstrap();
+}
+
+export {}; // mantém o arquivo como módulo
